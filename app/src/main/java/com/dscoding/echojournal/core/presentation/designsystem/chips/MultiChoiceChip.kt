78bee202f8ff772e5fde8b397d42311edf3f6f3b
diogocastrosantos <diogo.santos@id-pal.com>
2025-12-27 20:26:06 +0000

    Add DS Chip Components

---
+++ app/src/main/java/com/dscoding/echojournal/core/presentation/designsystem/chips/MultiChoiceChip.kt
@@ -0,0 +1,134 @@
+package com.dscoding.echojournal.core.presentation.designsystem.chips
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Build
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color.Companion.Transparent
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.dscoding.echojournal.R
+import com.dscoding.echojournal.core.presentation.designsystem.theme.EchoJournalTheme
+
+@Composable
+fun MultiChoiceChip(
+    text: String,
+    onClick: () -> Unit,
+    isClearVisible: Boolean,
+    onClearButtonClick: () -> Unit,
+    isHighlighted: Boolean,
+    isDropDownVisible: Boolean,
+    dropDownMenu: @Composable () -> Unit,
+    leadingContent: (@Composable () -> Unit)? = null,
+    modifier: Modifier = Modifier
+) {
+    val containerColor = if (isHighlighted) {
+        MaterialTheme.colorScheme.surface
+    } else {
+        Transparent
+    }
+
+    val borderColor = if (isHighlighted) {
+        MaterialTheme.colorScheme.primaryContainer
+    } else {
+        MaterialTheme.colorScheme.outline
+    }
+
+    Box(
+        modifier = modifier
+            .then(
+                if (isHighlighted) {
+                    Modifier.shadow(
+                        elevation = 4.dp,
+                        shape = CircleShape
+                    )
+                } else Modifier
+            )
+            .clip(CircleShape)
+            .border(
+                width = 0.5.dp,
+                color = borderColor,
+                shape = CircleShape
+            )
+            .background(containerColor)
+            .clickable(onClick = onClick)
+            .animateContentSize()
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(6.dp),
+            modifier = Modifier
+                .padding(8.dp)
+                .align(Alignment.Center)
+        ) {
+            leadingContent?.invoke()
+            Text(
+                text = text,
+                style = MaterialTheme.typography.labelLarge,
+                textAlign = TextAlign.Center,
+                color = MaterialTheme.colorScheme.secondary
+            )
+            AnimatedVisibility(
+                visible = isClearVisible
+            ) {
+                IconButton(
+                    onClick = onClearButtonClick,
+                    modifier = Modifier
+                        .size(16.dp)
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Clear,
+                        contentDescription = stringResource(id = R.string.clear_selection),
+                        tint = MaterialTheme.colorScheme.secondaryContainer
+                    )
+                }
+            }
+        }
+        if (isDropDownVisible) {
+            dropDownMenu()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun MultiChoiceChipPreview() {
+    EchoJournalTheme {
+        MultiChoiceChip(
+            text = "All topics",
+            onClick = {},
+            isClearVisible = true,
+            onClearButtonClick = { },
+            isHighlighted = true,
+            isDropDownVisible = true,
+            dropDownMenu = {},
+            leadingContent = {
+                Icon(
+                    imageVector = Icons.Default.Build,
+                    contentDescription = null
+                )
+            }
+        )
+    }
+}
